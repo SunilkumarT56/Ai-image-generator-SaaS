e6a109ddfa86012e5585a8451a31b8fb0f15f569
RATISH S <saravanankavitha876@gmail.com>
2025-09-28 15:12:54 +0530

    Small Changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@
 import React from 'react'
 import { useContext } from 'react'
 import {Routes,Route} from 'react-router-dom'
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Home from './pages/Home'
 import Result from './pages/Result'
 import BuyCredit from './pages/BuyCredit'
@@ -18,6 +20,7 @@ const App = () => {
   return (
     <div className='px-4 sm:px-10 md:px-14 lg:px-28
     min-h-screen bg-gradient-to-b from-teal-50 to-orange-50'>
+      <ToastContainer position='bottom-right'/>
       <Navbar/>
       {showLogin && <Login/>}
       <Routes>
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,5 +1,7 @@
-import { createContext } from "react";
+import { createContext, useEffect } from "react";
 import { useState } from "react";
+import {toast} from "react-toastify";
+import axios from 'axios';
 
 export const AppContext = createContext()
 
@@ -7,8 +9,39 @@ const AppContextProvider = (props) => {
     const [user,setUser]=useState(null);
     const [showLogin,setShowLogin]=useState(false);
 
+    const [token,setToken]=useState(localStorage.getItem('token'));
+
+    const [credit,setCredit]=useState(false)
+    const backendurl = import.meta.env.VITE_BACKEND_URL
+
+    const loadCreditsData = async()=>{
+        try {
+            const {data}= await axios.get(backendurl+'/api/user/credits', {headers: {token}})
+            if(data.status){
+                setCredit(data.credits)
+                setUser(data.user)
+            }
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
+
+    const logout=()=>{
+        localStorage.removeItem('token')
+        setToken('')
+        setUser(null)
+    }
+
+    useEffect(()=>{
+        if(token){
+            loadCreditsData()
+        }
+    },[token])
+
     const value={
-        user,setUser,showLogin,setShowLogin
+        user,setUser,showLogin,setShowLogin, backendurl,
+        token,setToken,credit,setCredit,loadCreditsData,logout
     }
 
     return(
